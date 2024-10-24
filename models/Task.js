38eda6fb61a51c50e56d7c741159d7adbc4e2b13
Nighthawks829 <liew_shai_bi21@iluv.ums.edu.my>
2024-10-24 21:25:34 +0800

    Created Tasks model in database

---
+++ models/Task.js
@@ -0,0 +1,66 @@
+const { DataTypes, Sequelize } = require("sequelize");
+const sequelize = require("../db/connect");
+
+const TaskSchema = sequelize.define(
+  "Tasks",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      primaryKey: true,
+      allowNull: false
+    },
+    userId: {
+      type: DataTypes.UUID,
+      allowNull: false
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          args: [2, 50],
+          msg: "Task titiel should be between 2 and 50 characters"
+        }
+      }
+    },
+    message: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
+    done: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW
+    }
+  },
+  {
+    timestamps: false,
+    freezeTableName: true,
+    hooks: {
+      beforeUpdate: async (task) => {
+        task.updatedAt = new Date();
+      }
+    }
+  }
+);
+
+sequelize
+  .sync()
+  .then(() => {
+    console.log("Tasks table created successfully");
+  })
+  .catch((error) => {
+    console.log("Unable to create Tasks table: ", error);
+  });
+
+module.exports = TaskSchema;
